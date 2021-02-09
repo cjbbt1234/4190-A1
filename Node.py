d266dd7bbd44bfe2b5b29a93bd8d720f24f09398
Yubo <starchen428@gmail.com>
2021-02-08 20:23:37 -0600

    "Discuss Node file"

--- Node.py
+++ Node.py
@@ -5,3 +5,15 @@ class Node:
         self.col=c
         # status: 0-empty 1-star 2-temporary flag 3-permanent flag
         self.status=0
+        self.neig=[]
+    def addNeighbor(self,n):
+        self.neig.append(n)
+    def changeToEmpty(self):
+        self.status=0
+    def changeToStar(self):
+        self.status=1
+    def changeToTflag(self):
+        self.status=2
+    def changeToPflag(self):
+        self.status=3
+
